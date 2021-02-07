7cf6806d7956b0c6d0f2564d115db674a525ef55
jd-create <jenniferdemeijer@t-online.de>
2021-02-07 18:26:45 +0100

    Class Address and Order deleted to restore to base

--- src/main/java/nl/novi/jdemeijervandriel/tailorism/domain/Address.java
+++
@@ -1,8 +0,0 @@
-package nl.novi.jdemeijervandriel.tailorism.domain;
-
-public class Address {
-    private Address address;
-    private String street;
-    private String houseNumber;
-    private String city;
-}
