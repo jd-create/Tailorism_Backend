890caa6add112c467290d57635ce11e4b53ce4a3
jd-create <jenniferdemeijer@t-online.de>
2021-02-14 09:36:03 +0100

    Made Product class

---
+++ src/main/java/nl/novi/jdemeijervandriel/tailorism/domain/Product.java
@@ -0,0 +1,59 @@
+package nl.novi.jdemeijervandriel.tailorism.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(columnDefinition = "serial")
+    private long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    public Product(){}
+
+    public Product(String name, String description, BigDecimal price){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
